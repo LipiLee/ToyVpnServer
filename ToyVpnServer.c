f587b9dfdd9562d719ef113d75fb9d2422507727
Lipi Lee <lipisoft@gmail.com>
2018-01-09 23:33:33 +0000

    cleanup

--- ToyVpnServer.c
+++ ToyVpnServer.c
@@ -13,53 +13,21 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
+#include <errno.h>
+#include <fcntl.h>
+#include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <arpa/inet.h>
+#include <linux/if_tun.h>
+#include <net/if.h>
 #include <netinet/in.h>
 #include <sys/ioctl.h>
 #include <sys/socket.h>
 #include <sys/stat.h>
 #include <sys/types.h>
-#include <errno.h>
-#include <fcntl.h>
-#include <pthread.h>
-
-//#include "logger.h"
-
-#ifdef __linux__
-
-// There are several ways to play with this program. Here we just give an
-// example for the simplest scenario. Let us say that a Linux box has a
-// public IPv4 address on eth0. Please try the following steps and adjust
-// the parameters when necessary.
-//
-// # Enable IP forwarding
-// echo 1 > /proc/sys/net/ipv4/ip_forward
-//
-// # Pick a range of private addresses and perform NAT over eth0.
-// iptables -t nat -A POSTROUTING -s 10.0.0.0/8 -o eth0 -j MASQUERADE
-//
-// # Create a TUN interface.
-// ip tuntap add dev tun0 mode tun
-//
-// # Set the addresses and bring up the interface.
-// ifconfig tun0 10.0.0.1 dstaddr 10.0.0.2 up
-//
-// # Create a server on port 8000 with shared secret "test".
-// ./ToyVpnServer tun0 8000 test -m 1400 -a 10.0.0.2 32 -d 8.8.8.8 -r 0.0.0.0 0
-//
-// This program only handles a session at a time. To allow multiple sessions,
-// multiple servers can be created on the same port, but each of them requires
-// its own TUN interface. A short shell script will be sufficient. Since this
-// program is designed for demonstration purpose, it performs neither strong
-// authentication nor encryption. DO NOT USE IT IN PRODUCTION!
-
-#include <net/if.h>
-#include <linux/if_tun.h>
 
 #define handle_error_en(en, msg) \
         do { errno = en; perror(msg); exit(EXIT_FAILURE); } while(0)
@@ -71,9 +39,7 @@ pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 static int get_interface(char *name)
 {
     const int interface = open("/dev/net/tun", O_RDWR);
-    if (interface == -1) {
-        handle_error("open");
-    }
+    if (interface == -1) handle_error("open");
 
     struct ifreq ifr;
     bzero(&ifr, sizeof(struct ifreq));
@@ -87,12 +53,6 @@ static int get_interface(char *name)
 
     return interface;
 }
-
-#else
-
-#error Sorry, you have to implement this part by yourself.
-
-#endif
 
 static int get_tunnel(char *port, char *secret)
 {
